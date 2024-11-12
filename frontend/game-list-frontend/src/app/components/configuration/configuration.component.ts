0c22c03d3ef46c3e944ee47f31fd2a2a44094f62
Lautaro Trabajo <lautaro.fuente@yahoo.com>
2024-11-12 11:08:40 -0300

    update menu

--- frontend/game-list-frontend/src/app/components/configuration/configuration.component.ts
+++ frontend/game-list-frontend/src/app/components/configuration/configuration.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ElectronService } from '../../services/electron.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-configuration',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './configuration.component.html',
   styleUrl: './configuration.component.css'
 })
