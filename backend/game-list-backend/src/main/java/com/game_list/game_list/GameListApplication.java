8e9d74f84098c80f249536e12a2869ed2214f4d1
LautaroFuente <lautaro.fuente@yahoo.com>
2025-11-26 17:25:02 -0300

    finish

--- backend/game-list-backend/src/main/java/com/game_list/game_list/GameListApplication.java
+++ backend/game-list-backend/src/main/java/com/game_list/game_list/GameListApplication.java
@@ -2,17 +2,11 @@ package com.game_list.game_list;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class GameListApplication {
 
 	public static void main(String[] args) {
-		
-		Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		
 		SpringApplication.run(GameListApplication.class, args);
 		System.out.println("Aplicacion BackEnd Funcionando");
--- main.js
+++ main.js
@@ -1,71 +1,89 @@
-const { app, BrowserWindow } = require('electron');
-const { ipcMain } = require('electron');
-const path = require('path');
-
+const { app, BrowserWindow } = require("electron");
+const { ipcMain } = require("electron");
+const path = require("path");
+const { spawn } = require("child_process");
 
 (async () => {
-    const Store = (await import('electron-store')).default;
-    const store = new Store();
+  const Store = (await import("electron-store")).default;
+  const store = new Store();
 
-
-const sizes = [
+  const sizes = [
     { width: 800, height: 600 },
     { width: 1024, height: 768 },
     { width: 1280, height: 720 },
-    { width: 1920, height: 1080 }
-];
+    { width: 1920, height: 1080 },
+  ];
 
-function createWindow(sizeIndex) {
+  let backendProcess;
+
+  function startBackend() {
+    const jarPath = path.join(
+      process.resourcesPath,
+      "backend",
+      "game-list.jar"
+    );
+
+    backendProcess = spawn("java", ["-jar", jarPath]);
+
+    backendProcess.stdout.on("data", (data) =>
+      console.log("[BACKEND]", data.toString())
+    );
+  }
+
+  function createWindow(sizeIndex) {
     const { width, height } = sizes[sizeIndex];
 
-    const win = new BrowserWindow({
-        width: width,
-        height: height,
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-            preload: path.join(__dirname, 'preload.js')
-        }
+    let win = new BrowserWindow({
+      width: width,
+      height: height,
+      webPreferences: {
+        nodeIntegration: false,
+        contextIsolation: true,
+        preload: path.join(__dirname, "preload.js"),
+      },
     });
 
-    win.loadURL('http://localhost:4200');
+    win.loadURL("http://localhost:8080");
 
-    win.on('closed', () => {
-        win = null;
+    win.on("closed", () => {
+      win = null;
     });
-}
+  }
 
-app.whenReady().then(() => {
+  app.whenReady().then(() => {
+    startBackend();
     createWindow(0);
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow(0);
-        }
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow(0);
+      }
     });
-});
+  });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
     }
-});
+  });
 
-ipcMain.on('get-data', (event, key) => {
+  app.on("before-quit", () => {
+    if (backendProcess) backendProcess.kill();
+  });
+
+  ipcMain.on("get-data", (event, key) => {
     const value = store.get(key);
-    event.reply('send-data', value);
-});
+    event.reply("send-data", value);
+  });
 
-ipcMain.on('set-data', (event, key, value) => {
+  ipcMain.on("set-data", (event, key, value) => {
     store.set(key, value);
-});
+  });
 
-ipcMain.on('resize-window', (event, { width, height }) => {
+  ipcMain.on("resize-window", (event, { width, height }) => {
     const win = BrowserWindow.getFocusedWindow();
     if (win) {
       win.setSize(width, height);
     }
-});
-  
-  })();
-  
+  });
+})();
